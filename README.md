251f2babbb27c281f3ce08355118cc966c0a3fbe
Lee Chun On <jeffreyleeon@gmail.com>
2016-07-14 22:15:22 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Example from http://androidexample.com/Create_A_Simple_Listview_-_Android_Example/index.php?view=article_discription&aid=65&aaid=90
---
+++ app/src/main/java/com/jeffreyleeon/listviewexample/MainActivity.java
@@ -0,0 +1,46 @@
+package com.jeffreyleeon.listviewexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+        // Define Array values to show in ListView
+        String[] values = new String[] {
+                "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.simple_list_item_1,
+                R.id.text1,
+                values
+        );
+        listView.setAdapter(adapter);
+    }
+}
